ad20b24147ef29b9160374870c5eb1f2179fa0fb
grszik <99998467+grszik@users.noreply.github.com>
2026-04-30 17:38:48 +0200

    update

--- src/main.cpp
+++ src/main.cpp
@@ -149,6 +149,7 @@ int main() {
       if(IsKeyPressed(KEY_R)) {
         play = true;
         turn = false;
+        win = 0;
         for(int i = 0; i<3; i++) {
           for(int j = 0; j<3; j++) {
             matrix[i][j] = 0;
